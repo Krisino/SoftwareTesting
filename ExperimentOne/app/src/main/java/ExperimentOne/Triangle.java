de4300033aff1f820bd29e3bb5e729dc3c5060f3
Krisino <1309769300@qq.com>
2022-03-07 19:17:08 +0800

    Experiment 1 Content Complement

--- ExperimentOne/app/src/main/java/ExperimentOne/Triangle.java
+++ ExperimentOne/app/src/main/java/ExperimentOne/Triangle.java
@@ -1,3 +1,5 @@
+package ExperimentOne;
+
 public class Triangle {
     public String classify(int a, int b, int c) {
         if (a <1 || a>100 || b<1 || b>100 || c<1 || c> 100) {
@@ -8,7 +10,7 @@ public class Triangle {
         } else if (a == b && a == c && b == c) {
             return "等边三角形";
         } else if (a != b && a != c && b != c) {
-            return "不等边三角形";
+            return "一般三角形";
         } else {
             return "等腰三角形";
         }
--- ExperimentOne/app/src/test/java/ExperimentOne/TriangleTest.java
+++ ExperimentOne/app/src/test/java/ExperimentOne/TriangleTest.java
@@ -1,12 +1,15 @@
+package ExperimentOne;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /***
- * 
+ *
  * @author Krisino
  * @date 2022/3/7-16:42:13
- * @description 软件测试实验一：三角形测试用例
+ * @description SoftwareTesting: Experiment One: Triangle
  */
 public class TriangleTest {
 
@@ -19,10 +22,35 @@ public class TriangleTest {
     }
 
     @Test
-    @DisplayName("不等边三角形")
+    @DisplayName("非三角形")
+    void not_triangle() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(1, 1, 3);
+        assertEquals("非三角形", type);
+    }
+
+    @Test
+    @DisplayName("一般三角形")
     void scalene_test() {
         Triangle triangle = new Triangle();
         String type = triangle.classify(3, 4, 6);
-        assertEquals("不等边三角形", type);
+        assertEquals("一般三角形", type);
     }
+
+    @Test
+    @DisplayName("等边三角形")
+    void equilateral_triangle() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(1, 1, 1);
+        assertEquals("等边三角形", type);
+    }
+
+    @Test
+    @DisplayName("等腰三角形")
+    void isosceles_triangle() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(2, 2, 1);
+        assertEquals("等腰三角形", type);
+    }
+
 }
