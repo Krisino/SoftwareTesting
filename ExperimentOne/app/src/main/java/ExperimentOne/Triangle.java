aa4548d7eaa010b648361a3a7906eaacbc9eebce
Krisino <1309769300@qq.com>
2022-03-10 10:38:34 +0800

    Refine the test code

--- ExperimentOne/app/src/main/java/ExperimentOne/Triangle.java
+++ ExperimentOne/app/src/main/java/ExperimentOne/Triangle.java
@@ -2,17 +2,17 @@ package ExperimentOne;
 
 public class Triangle {
     public String classify(int a, int b, int c) {
-        if (a <1 || a>100 || b<1 || b>100 || c<1 || c> 100) {
-            return "输入错误";
-        } 
+        if (a < 1 || a > 100 || b < 1 || b > 100 || c < 1 || c > 100) {
+            return "Illegal Input";
+        }
         if (!((a + b > c) && (a + c > b) && (b + c > a))) {
-            return "非三角形";
+            return "Not Triangle";
         } else if (a == b && a == c && b == c) {
-            return "等边三角形";
+            return "Equilateral triangle";
         } else if (a != b && a != c && b != c) {
-            return "一般三角形";
+            return "General Triangle";
         } else {
-            return "等腰三角形";
+            return "Isosceles Triangle";
         }
     }
 }
--- ExperimentOne/app/src/test/java/ExperimentOne/TriangleTest.java
+++ ExperimentOne/app/src/test/java/ExperimentOne/TriangleTest.java
@@ -13,44 +13,165 @@ import org.junit.jupiter.api.Test;
  */
 public class TriangleTest {
 
+
     @Test
-    @DisplayName("输入错误")
-    void parameters_error_test() {
+    @DisplayName("Isosceles Triangle")
+    void general_Triangle_1() {
         Triangle triangle = new Triangle();
-        String type = triangle.classify(0, 4, 5);
-        assertEquals("输入错误", type);
+        String type = triangle.classify(1, 50, 50);
+        assertEquals("Isosceles Triangle", type);
     }
 
     @Test
-    @DisplayName("非三角形")
-    void not_triangle() {
+    @DisplayName("Isosceles Triangle")
+    void general_Triangle_2() {
         Triangle triangle = new Triangle();
-        String type = triangle.classify(1, 1, 3);
-        assertEquals("非三角形", type);
+        String type = triangle.classify(2, 50, 50);
+        assertEquals("Isosceles Triangle", type);
     }
 
     @Test
-    @DisplayName("一般三角形")
-    void scalene_test() {
+    @DisplayName("Equilateral triangle")
+    void equilateral_triangle_1() {
         Triangle triangle = new Triangle();
-        String type = triangle.classify(3, 4, 6);
-        assertEquals("一般三角形", type);
+        String type = triangle.classify(50, 50, 50);
+        assertEquals("Equilateral triangle", type);
     }
 
     @Test
-    @DisplayName("等边三角形")
-    void equilateral_triangle() {
+    @DisplayName("Isosceles Triangle")
+    void general_Triangle_3() {
         Triangle triangle = new Triangle();
-        String type = triangle.classify(1, 1, 1);
-        assertEquals("等边三角形", type);
+        String type = triangle.classify(99, 50, 50);
+        assertEquals("Isosceles Triangle", type);
     }
 
     @Test
-    @DisplayName("等腰三角形")
-    void isosceles_triangle() {
+    @DisplayName("Not Triangle")
+    void not_triangle_1() {
         Triangle triangle = new Triangle();
-        String type = triangle.classify(2, 2, 1);
-        assertEquals("等腰三角形", type);
+        String type = triangle.classify(100, 50, 50);
+        assertEquals("Not Triangle", type);
+    }
+
+    @Test
+    @DisplayName("Isosceles Triangle")
+    void general_Triangle_4() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(50, 1, 50);
+        assertEquals("Isosceles Triangle",   type);
+    }
+
+    @Test
+    @DisplayName("Isosceles Triangle")
+    void general_Triangle_5() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(50, 2, 50);
+        assertEquals("Isosceles Triangle", type);
+    }
+
+    @Test
+    @DisplayName("Isosceles Triangle")
+    void general_Triangle_6() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(50, 99, 50);
+        assertEquals("Isosceles Triangle", type);
+    }
+
+    @Test
+    @DisplayName("Not Triangle")
+    void not_triangle_2() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(50, 100, 50);
+        assertEquals("Not Triangle", type);
+    }
+
+    @Test
+    @DisplayName("Isosceles Triangle")
+    void general_Triangle_7() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(50, 50, 1);
+        assertEquals("Isosceles Triangle", type);
+    }
+
+    @Test
+    @DisplayName("Isosceles Triangle")
+    void general_Triangle_8() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(50, 50, 2);
+        assertEquals("Isosceles Triangle", type);
+    }
+
+    @Test
+    @DisplayName("Isosceles Triangle")
+    void general_Triangle_9() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(50, 50, 99);
+        assertEquals("Isosceles Triangle", type);
+    }
+
+    @Test
+    @DisplayName("Not Triangle")
+    void not_triangle_3() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(50, 50, 100);
+        assertEquals("Not Triangle", type);
+    }
+
+    @Test
+    @DisplayName("Illegal Input")
+    void illegal_input_1() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(0, 50, 50);
+        assertEquals("Illegal Input", type);
+    }
+
+    @Test
+    @DisplayName("Illegal Input")
+    void illegal_input_2() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(101, 50, 50);
+        assertEquals("Illegal Input", type);
+    }
+
+    @Test
+    @DisplayName("Illegal Input")
+    void illegal_input_3() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(50, 0, 50);
+        assertEquals("Illegal Input", type);
+    }
+
+    @Test
+    @DisplayName("Illegal Input")
+    void illegal_input_4() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(50, 101, 50);
+        assertEquals("Illegal Input", type);
+    }
+
+    @Test
+    @DisplayName("Illegal Input")
+    void illegal_input_5() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(50, 50, 0);
+        assertEquals("Illegal Input", type);
+    }
+
+    @Test
+    @DisplayName("Illegal Input")
+    void illegal_input_6() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(50, 50, 101);
+        assertEquals("Illegal Input", type);
+    }
+
+    @Test
+    @DisplayName("General Triangle")
+    void general_triangle_1() {
+        Triangle triangle = new Triangle();
+        String type = triangle.classify(49, 50, 51);
+        assertEquals("General Triangle", type);
     }
 
 }
